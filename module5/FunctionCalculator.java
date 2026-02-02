022f7835f999ce59ced200c157991f2a82548d5a
Akhlaq Ahemad <akhlaqahemad6@gmail.com>
2026-02-02 13:10:20 +0530

    Calculator Via Function

---
+++ module5/FunctionCalculator.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class FunctionCalculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Division by zero is not allowed");
+            return 0;
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Choose operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Result = " + add(a, b));
+                break;
+            case 2:
+                System.out.println("Result = " + subtract(a, b));
+                break;
+            case 3:
+                System.out.println("Result = " + multiply(a, b));
+                break;
+            case 4:
+                System.out.println("Result = " + divide(a, b));
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+    }
+}
