5c5aecf4aae0cc2ffb10ee681d579fe01ed992f3
Akhlaq Ahemad <akhlaqahemad6@gmail.com>
2026-02-02 16:58:59 +0530

    Neither Parameter Nor Return Via Function Program

---
+++ module5/NoParamNoReturn.java
@@ -0,0 +1,10 @@
+public class NoParamNoReturn {
+
+    public static void main(String[] args) {
+        showMessage();   // function call
+    }
+
+    public static void showMessage() {
+        System.out.println("Welcome to Java Programming");
+    }
+}
