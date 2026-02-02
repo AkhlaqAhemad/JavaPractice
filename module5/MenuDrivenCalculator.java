f6021ca0dc12676c130522ae70f05f779f10bd0f
Akhlaq Ahemad <akhlaqahemad6@gmail.com>
2026-02-02 14:53:27 +0530

    Menu Driven Function Program

---
+++ module5/MenuDrivenCalculator.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class MenuDrivenCalculator {
+
+    // Function for Addition
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Function for Subtraction
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Function for Multiplication
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Function for Division
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Division by zero not allowed");
+            return 0;
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = sc.nextInt();
+
+            if (choice >= 1 && choice <= 4) {
+                System.out.print("Enter first number: ");
+                int a = sc.nextInt();
+
+                System.out.print("Enter second number: ");
+                int b = sc.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        System.out.println("Result = " + add(a, b));
+                        break;
+                    case 2:
+                        System.out.println("Result = " + subtract(a, b));
+                        break;
+                    case 3:
+                        System.out.println("Result = " + multiply(a, b));
+                        break;
+                    case 4:
+                        System.out.println("Result = " + divide(a, b));
+                        break;
+                }
+            } else if (choice != 5) {
+                System.out.println("Invalid choice");
+            }
+
+        } while (choice != 5);
+
+        System.out.println("Program exited.");
+    }
+}
